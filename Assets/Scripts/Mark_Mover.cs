b4353b924a5e6d2db6568768cc1f2032509d138d
Max Sobhani <msobhani97@outlook.com>
2017-08-28 14:52:41 -0500

    Journal_A1
    Created Journal, Notes, and Maps

---
+++ Assets/Scripts/Mark_Mover.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Mark_Mover : MonoBehaviour, IPointerDownHandler {
+
+	public void OnPointerDown (PointerEventData eventData)
+	{
+		if (eventData.button == PointerEventData.InputButton.Right) 
+		{
+			Destroy (gameObject);
+		}
+	}
+}
