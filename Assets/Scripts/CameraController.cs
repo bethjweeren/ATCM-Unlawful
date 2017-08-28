99a3bb80f9fad092cd6da1dac69e92388a61174a
SharonLougheed <sharon.lougheed@gmail.com>
2017-08-27 22:46:39 -0500

    Player Movement
    Adds a PlayerController script, a small CameraController script, the player sprites, player animation files, a test sprite, and a scene to test player movement.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+	public GameObject player;
+
+	// Use this for initialization
+	void Start ()
+	{
+		this.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, this.transform.position.z);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		this.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, this.transform.position.z);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Controls player movement and handles player sprite animations
+public class PlayerController : MonoBehaviour
+{
+	// Different screens a player can be in
+	// Was originally going to have "still" and "walking" but there was too much overlap
+	enum State
+	{
+		MAIN,
+		MAP,
+		JOURNAL,
+		INVENTORY,
+		INTERACTING
+	};
+
+	public float speed = 1;
+	private State currentState;
+	private Rigidbody2D playerRB;
+	private Animator animator;
+
+	// Use this for initialization
+	void Start()
+	{
+		currentState = State.MAIN;
+		playerRB = this.GetComponent<Rigidbody2D>();
+		animator = this.GetComponent<Animator>();
+	}
+
+	// Update is called every fixed framerate frame
+	void FixedUpdate()
+	{
+		switch (currentState)
+		{
+			case State.MAIN:
+				animator.enabled = true; //Animate sprite
+				// It checks every frame if a key is held down and sets velocity and animation accordingly
+				// ...Even if both were already set, which isn't ideal I think.
+				if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+				{
+					animator.SetInteger("Direction", 0); //Animate north sprite
+					playerRB.velocity = new Vector2(0, 1) * speed; //Move north
+				}
+				else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+				{
+					animator.SetInteger("Direction", 1); //Animate east sprite
+					playerRB.velocity = new Vector2(1, 0) * speed; //Move east
+				}
+				else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+				{
+					animator.SetInteger("Direction", 2); //Animate south sprite
+					playerRB.velocity = new Vector2(0, -1) * speed; //Move south
+				}
+				else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+				{
+					animator.SetInteger("Direction", 3); //Animate west sprite
+					playerRB.velocity = new Vector2(-1, 0) * speed; //Move west
+				}
+				else
+				{
+					animator.enabled = false; //Stop animating sprite
+					playerRB.velocity = new Vector2(0, 0); //Don't move
+
+					if (Input.GetKeyDown(KeyCode.M))
+					{
+						currentState = State.MAP;
+						print("Looking at map. Press M or ESC or Space to close."); //Replace with map code
+					}
+					else if (Input.GetKeyDown(KeyCode.J))
+					{
+						currentState = State.JOURNAL;
+						print("Looking at journal. Press ESC to close."); //Replace with journal code
+					}
+					else if (Input.GetKeyDown(KeyCode.I))
+					{
+						currentState = State.INVENTORY;
+						print("Looking through inventory. Press I or ESC to close."); //Replace with inventory code
+					}
+					else if (Input.GetKeyDown(KeyCode.E) || Input.GetKeyDown(KeyCode.Space))
+					{
+						currentState = State.INTERACTING;
+						print("Looking or talking. Press E or Space or ESC to end interaction."); //Replace with interaction code
+					}
+				}
+				break;
+			case State.MAP:
+				if (Input.GetKeyDown(KeyCode.M) || Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.Space))
+				{
+					currentState = State.MAIN;
+					print("Closed map"); //Replace with map code
+				}
+				break;
+			case State.JOURNAL:
+				if (Input.GetKeyDown(KeyCode.Escape)) //Pressing "J" to close the journal might not work if typing
+				{
+					currentState = State.MAIN;
+					print("Closed journal."); //Replace with journal code
+				}
+				break;
+			case State.INVENTORY:
+				if (Input.GetKeyDown(KeyCode.I) || Input.GetKeyDown(KeyCode.Escape))
+				{
+					currentState = State.MAIN;
+					print("Closed inventory."); //Replace with inventory code
+				}
+				break;
+			case State.INTERACTING:
+				if (Input.GetKeyDown(KeyCode.E) || Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Escape))
+				{
+					currentState = State.MAIN;
+					print("Ended interaction."); //Replace with interaction/NPC discussion code
+				}
+				break;
+		}
+	}
+}
