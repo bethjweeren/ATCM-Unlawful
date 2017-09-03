f8cb046e8d8773bf2a8cb836beb06c6dd2135d83
SharonLougheed <sharon.lougheed@gmail.com>
2017-09-03 16:33:28 -0500

    Menu and Started on Camera Switching
    Added menu, controls, and credits screens. Also started on camera switching and made some test buildings.

---
+++ Assets/Scripts/ButtonQuit.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ButtonQuit : MonoBehaviour
+{
+	public Button yourButton;
+
+	void Start()
+	{
+		Button btn = yourButton.GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+	}
+
+	void TaskOnClick()
+	{
+		Application.Quit();
+	}
+}
