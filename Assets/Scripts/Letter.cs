bf036cb36eb5424d9351be7b4d54f456dcb04b16
Grant <grantcbarbee@users.noreply.github.com>
2017-10-17 01:25:37 -0500

    I win this time, tutorial letter...

---
+++ Assets/Scripts/Letter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Letter : MonoBehaviour, IInteractable {
+    public GameObject letterUI;
+
+    public void Interact()
+    {
+        PlayerController player = Provider.GetInstance().player;
+        player.currentState = PlayerController.State.INTERACTING;
+        player.StopMoving();
+        letterUI.SetActive(true);
+        Destroy(this.gameObject);
+    }
+}
