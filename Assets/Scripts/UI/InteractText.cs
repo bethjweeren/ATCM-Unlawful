ef272dbacc10861aa13122c4e0f4f774378e7d03
SharonLougheed <sharon.lougheed@gmail.com>
2017-11-06 03:48:59 -0600

    Text Backgrounds
    Added background images for the letter interact text and inventory description text.

--- Assets/Scripts/UI/InteractText.cs
+++ Assets/Scripts/UI/InteractText.cs
@@ -5,15 +5,18 @@ using UnityEngine;
 public class InteractText : MonoBehaviour {
 
 	public GameObject interactText;
+	public GameObject interactTextBG;
 
 	void OnTriggerEnter2D(Collider2D other) {
 		if(other.tag == "Player"){
+			interactTextBG.SetActive(true);
 			interactText.SetActive(true);
 		}
 	}
 	
 	void OnTriggerExit2D(Collider2D other) {
 		if(other.tag == "Player"){
+			interactTextBG.SetActive(false);
 			interactText.SetActive(false);
 		}
 	}
