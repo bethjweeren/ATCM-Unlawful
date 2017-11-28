fe2d9182a53211676c51cd7bd32308fbe1f71794
SharonLougheed <sharon.lougheed@gmail.com>
2017-11-28 16:09:14 -0600

    Credits
    Max's changes to credits, including photos and bios of people on the team.

--- Assets/Scripts/UI/ItemUI_Image.cs
+++ Assets/Scripts/UI/ItemUI_Image.cs
@@ -9,15 +9,33 @@ public class ItemUI_Image : MonoBehaviour, IPointerEnterHandler, IPointerExitHan
 	public Image itemTextBG;
 	public Text itemText;
 	public string desc;
+    public GameObject picture;
+    public bool isDesc;
+
+    void Start()
+    {
+        if (isDesc)
+        {
+            picture.SetActive(false);
+        }
+    }
 
 	public void OnPointerEnter (PointerEventData eventData){
 		itemTextBG.enabled = true;
 		itemText.text = desc;
+        if (isDesc)
+        {
+            picture.SetActive(true);
+        }
 	}
 
 	public void OnPointerExit (PointerEventData eventData){
 		itemTextBG.enabled = false;
 		itemText.text = "";
-	}
+        if (isDesc)
+        {
+            picture.SetActive(false);
+        }
+    }
 
 }
